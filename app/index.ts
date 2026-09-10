65c3bcdec49a98b3022e765f2fa298e39f48ea0c
Binanky <douglaswaltrich123@gmail.com>
2026-09-10 16:34:47 -0300

    Corrigir ponto de entrada Express da Vercel

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,7 @@
-export { default } from "./server/_core/index";
+import express from "express";
+import app from "./server/_core/index";
+
+// A importação explícita permite que o detector da Vercel reconheça este arquivo como uma aplicação Express.
+void express;
+
+export default app;
